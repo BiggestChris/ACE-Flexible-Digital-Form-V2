bb22743294874cf419fcd9a458b01b12aab9684e
BiggestChris <stylianouchristopher8@gmail.com>
2023-12-19 07:01:01 +0000

    Testing with extra script

---
+++ backend.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove, query, orderByChild } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+    databaseURL: "https://raf-ace-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+
+// Details on database to manage field information for webform itself
+const fieldInfo = ref(database, "Field-info");
+const uploadBtn = document.getElementById("upload-btn");
+const fieldOrder = document.getElementById("field-order");
+const fieldName = document.getElementById("field-name");
+const fieldType = document.getElementsByName("field-type");
+
+// Iterate through radio buttons to pull value of one selected
+let selectedValue;
+let selectedType;
+for (const selectedFieldType of fieldType) {
+  if (selectedFieldType.checked) {
+    selectedValue = selectedFieldType.value;
+    selectedType = selectedFieldType;
+    break;
+  }
+}
+
+uploadBtn.addEventListener("click", function() {
+    let orderValue = fieldOrder.value;
+    let fieldValue = fieldName.value;
+    let typeValue = selectedValue;
+    
+    push(fieldInfo, {
+        order: orderValue,
+        field: fieldValue,
+        type: typeValue
+    });
+    
+    fieldOrder.value = "";
+    fieldName.value = "";
+    selectedType.checked = false;
+
+})
