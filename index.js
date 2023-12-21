0dc50eff24a99196e0f0f5c5405952897e18f986
BiggestChris <stylianouchristopher8@gmail.com>
2023-12-21 07:06:56 +0000

    Pulling in new fields into front-end site

--- index.js
+++ index.js
@@ -142,6 +142,75 @@ function timestamp() {
     return time;
 }
 
+// Code to pull out the fields from the database to populate
+
+const databaseFields = document.getElementById("database-fields");
+const fieldInfo = ref(database, "Field-info");
+
+// Pull out the list of fields currently in the database
+
+const sortedQuery = query(fieldInfo, orderByChild('order'));
+
+onValue(fieldInfo, function(snapshot) {
+    
+    let itemsArray = Object.entries(snapshot.val());
+    itemsArray.sort(compareByOrder);
+    
+    
+    databaseFields.innerHTML = ""
+    
+    function compareByOrder(a, b) {
+        return a[1].order - b[1].order;
+    }
+    
+    /* let dummyArray = [];
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        dummyArray.push(Number(itemsArray[i][1].order));
+    } */
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        let item = itemsArray[i][1]
+             
+        // Create a wrapper div for field-category and the Remove button
+        const wrapperDiv = document.createElement("div");
+        wrapperDiv.classList.add("wrapper-div");
+
+        // Append field-category to the wrapper div
+        const fieldCategoryDiv = document.createElement("div");
+        fieldCategoryDiv.classList.add("field-category");
+        fieldCategoryDiv.innerHTML = 
+        `
+        <div class="sub-field field-key"><p class="sub-field-descriptor">field-key:</p><p id="current-item-unique-key">${itemsArray[i][0]}</p></div>
+        <div class="indent">
+            <div  class="sub-field"><p class="sub-field-descriptor">order #:</p><p class="current-item">${item.order}</p></div>
+            <div  class="sub-field"><p class="sub-field-descriptor">name:</p><p class="current-item">${item.field}</p></div>
+            <div  class="sub-field"><p class="sub-field-descriptor">price:</p><p class="current-item">${item.type}</p></div>
+        </div>
+        `
+        wrapperDiv.appendChild(fieldCategoryDiv);
+        
+        // Create and append the Remove button to the wrapper div
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "remove";
+        removeButton.classList.add("remove-btn");
+        wrapperDiv.appendChild(removeButton);
+        
+        // Append the wrapper div to the currentFields div
+        databaseFields.appendChild(wrapperDiv);
+        
+        removeButton.addEventListener("click", function() {
+            let exactLocationOfItemInDB = ref(database, `Field-info/${itemsArray[i][0]}`)
+            
+            remove(exactLocationOfItemInDB);
+        })
+    }
+})
+
+
+
+
+
 
 
 
